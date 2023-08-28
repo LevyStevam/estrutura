0f51fe5cd7bb947dc9906bcd8aa347fb807d8acb
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2023-08-28 11:35:49 -0300

    Adicionando outras coisas

--- ed.py
+++ ed.py
@@ -35,6 +35,7 @@ class LinkedList:
             node_aux = node_aux.next
         node_aux.next = node
         self.length += 1
+        
     def insertPosisition(self, data, pos):
         if pos > self.length:
             return 'posição invalida'
@@ -47,14 +48,82 @@ class LinkedList:
         node.next = node_aux.next
         node_aux.next = node
         self.length += 1
+        
+    def fullForce(self,n):
+        position = 0
+        node_aux = self.head
+        while node_aux.next != None:
+            position += 1
+            node_aux = node_aux.next
+        target = position - (n + 1)
+        count = 0
+        node_aux = self.head
+        while count <= target:
+            node_aux = node_aux.next
+            count += 1
+        
+        return node_aux.data
+        
+    
+    def fastslow(self, n):
+        fast =  slow = self.head
+        
+        count = 0
+        
+        while count < n:
+            count +=1
+            fast = fast.next
+        
+        while fast.next != None:
+            fast = fast.next
+            slow = slow.next
+        
+        return slow.data
+        
+    
+    def printarinverso(self, node = None):
+        if node == None:
+            node = self.head
+            
+        if node.next != None:
+            self.printarinverso(node.next)
+        
+        print(node.data)
+        
+    def insertInOrder(self, data):
+        
+        if self.head == None:
+            return
+        
+        if data < self.head.data:
+            return self.insertBeginning(data)
+        
+        current = self.head
+        position = 0
+        while current.next != None:
+            if data > current.data and data <= current.next.data:
+                return self.insertPosisition(data, position + 1)
+                
+            position += 1
+            current = current.next
+    
+    
+    
 
 lista_encadeada = LinkedList()
-lista_encadeada.insertBeginning('levy')
-lista_encadeada.insertBeginning('jorge')
-lista_encadeada.insertBeginning('careca')
-lista_encadeada.insertBeginning('jon')
-lista_encadeada.insertEnd('barros')
-lista_encadeada.insertEnd('barros')
-lista_encadeada.insertPosisition(3,5)
+lista_encadeada.insertBeginning(12)
+lista_encadeada.insertBeginning(9)
+lista_encadeada.insertBeginning(8)
+lista_encadeada.insertBeginning(8)
+lista_encadeada.insertEnd(15)
+lista_encadeada.insertBeginning(1)
+#lista_encadeada.insertInOrder(10)
+lista_encadeada.insertPosisition(5,9)
+
 lista_encadeada.printList()
-print(lista_encadeada.getLength())
+print('\n')
+print(lista_encadeada.fullForce(5))
+print(lista_encadeada.fastslow(5))
+print('\n')
+print('\n')
+lista_encadeada.printarinverso()
